f66eb83a15d556c8a6872044bf342ab5c87ff48e
Nuvole-Art <nuvole.art@bk.ru>
2024-05-25 12:57:44 +0300

    upload

---
+++ data.js
@@ -0,0 +1,269 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Прихожая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4824336702425438
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.981883357665799,
+          "pitch": 0.5689270910901385,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.8938700148440013,
+          "pitch": 0.6051483633232841,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.11189377122557076,
+          "pitch": 0.22835724021691206,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.2769029995164125,
+          "pitch": 0.45264133036188525,
+          "rotation": 0,
+          "target": "3---1"
+        },
+        {
+          "yaw": -0.29310200025543764,
+          "pitch": 0.1905041981509079,
+          "rotation": 0,
+          "target": "4---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.9436940315033162,
+        "pitch": 0.4357944465359047,
+        "fov": 1.4824336702425438
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6234417612124687,
+          "pitch": 0.645949351000036,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Гардероб",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.1570355244718034,
+        "pitch": 0.46969656913974944,
+        "fov": 1.4824336702425438
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7977040699876365,
+          "pitch": 0.5938526565808324,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---1",
+      "name": "Кухня-гостиная 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.2029320674390398,
+        "pitch": 0.5569860741565478,
+        "fov": 1.4824336702425438
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8149788663899713,
+          "pitch": 0.3065129440940755,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.1377246469127922,
+          "pitch": 0.5194587283403109,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 3.1226055360185914,
+          "pitch": 0.39262007066883875,
+          "rotation": 0,
+          "target": "4---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---2",
+      "name": "Кухня-гостиная 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.0266915263686478,
+        "pitch": 0.2720628688266977,
+        "fov": 1.4824336702425438
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5672062695621136,
+          "pitch": 0.4721607171012714,
+          "rotation": 0,
+          "target": "3---1"
+        },
+        {
+          "yaw": 1.2112084388782343,
+          "pitch": 0.2521613801998228,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.0872388609714587,
+          "pitch": 0.07354888214540978,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "Спальня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.01601491582118797,
+        "pitch": 0.2697890807443475,
+        "fov": 1.4824336702425438
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0635642056173342,
+          "pitch": 0.3010201828411301,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.5447087471495076,
+          "pitch": 0.20606139695998849,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
